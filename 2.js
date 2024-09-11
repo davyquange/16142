daaf924501750d1aaf565f59cdd702960717edcd
davyquange <826118993@qq.com>
2024-09-11 09:44:32 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,18 +1,1 @@
-
-let body = $response.body;
-
-let jsonBody = JSON.parse(body);
-jsonBody.vnjoy = {
-    "at": 1,
-    "as": 1,
-    "grade": {
-        "exp": 150,
-        "level": 9,
-        "icon": "https://hqres.51touxiang.com/hj_res/Ft4SW1DZf9Xh9AIFU1gj5WDYQ0DR.png?sign=cc279a57a17fc67199c0bd44fef21a1b&t=66dc7d33"
-    },
-    "autoRenewal": 0,
-    "expireTime": 4092595200000,
-    "expireDate": "2099-09-09"
-};
-
-$done({ body: JSON.stringify(jsonBody) });
+['jsjiami.com.v4']["\x66\x69\x6c\x74\x65\x72"]["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"](((['jsjiami.v4']+[])["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"]['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65']['\x61\x70\x70\x6c\x79'](null,"118j97q114u32M98H111k100L121R32o61d32U36m114F101r115t112r111D110X115D101x46N98k111n100z121R59B10x10O98K111u100H121E32a61M32M98h111c100Y121k46o114I101r112s108K97b99D101q40e47d34m97Y116n34F58Z92I100P43q47d103i44R32i39c34y97w116B34M58R49Y44P34i101A120L112O105C114v101M68v97s116L101B34E58V34G50B48v50t53a45F48t54V45e51k48k34u39V41v59i10N10h36Y100P111U110v101r40T123r32F98p111u100T121R32W125o41B59"['\x73\x70\x6c\x69\x74'](/[a-zA-Z]{1,}/))))('jsjiami.com.v4');
